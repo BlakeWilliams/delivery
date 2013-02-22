39e585207f61dbd190e5930af1683b6d744f1a7c
Blake Williams <blake@bendystra.ws>
2013-02-22 17:53:55 -0600

    Assume real config is on the server when deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,9 @@ namespace :deploy do
   task :symlink_config, roles: :app do
     run "rm #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+
+    run "rm #{release_path}/config/config.yml"
+    run "ln -nfs #{shared_path}/config/config.yml #{release_path}/config/config.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 end
